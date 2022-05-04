ab66ccefd48df16b762090c6f4ace391a4f38aa9
John Doe <john.doe@isae-supaero.fr>
2022-05-04 17:32:01 +0200

    Modification

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'topoguide.urls'
